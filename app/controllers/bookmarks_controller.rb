94ee619858a7ff916cd0ef603c63c616a4714053
Patricia Lambertz <lambertz.patricia@gmail.com>
2023-02-23 18:11:20 +0100

    all actions complete, except delete doesnt work quite well, design is missing

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,26 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    @bookmark.save
+    redirect_to lists_path, notice: "Bookmark was successfully created."
+  end
+
+  def delete
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:movie_id, :comment)
+  end
+end
